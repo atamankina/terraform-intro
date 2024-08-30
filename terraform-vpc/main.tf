3ed5ba5d1c37b7d7b8d1d9eb4c24d08b82eb0650
Galina Atamankina <g.atamankina@gmail.com>
2024-08-30 12:28:52 +0200

    Adding private subnet, public route table and public subnet association.

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -26,9 +26,43 @@ resource "aws_subnet" "main_public_subnet_a_prod" {
     vpc_id = aws_vpc.main_vpc_prod.id
     cidr_block = "10.0.0.0/20"
     availability_zone = "eu-central-1a"
+    map_public_ip_on_launch = true
 
     tags = {
         Name = "main-prod-public-subnet-a"
     }
   
+}
+
+# Private subnet
+resource "aws_subnet" "main_private_subnet_a_prod" {
+    vpc_id = aws_vpc.main_vpc_prod.id
+    cidr_block = "10.0.128.0/20"
+    availability_zone = "eu-central-1a"
+
+    tags = {
+        Name = "main-prod-private-subnet-a"
+    }
+  
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rtb_prod" {
+    vpc_id = aws_vpc.main_vpc_prod.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main_igw_prod.id
+    }
+
+    tags = {
+        Name = "main-prod-vpc-public-route-table"
+    }
+  
+}
+
+# Public Subnet to Public Route Table Association
+resource "aws_route_table_association" "public_rtb_subnet_assoc_prod" {
+    subnet_id = aws_subnet.main_public_subnet_a_prod.id
+    route_table_id = aws_route_table.public_rtb_prod.id
 }
